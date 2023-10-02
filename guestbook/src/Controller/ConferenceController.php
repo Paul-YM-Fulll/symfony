9efd8a7a7c0e345edc1cfc85b69e15e66304ddbf
Paul Yves-Menager <paul.yves-menager@fulll.fr>
2023-10-02 12:00:25 +0000

    easter egg

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -3,16 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'app_conference')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function index(Request $request): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
-    }
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        {
+            return new Response(<<<EOF
+                <html>
+                    <body style="display:flex;justify-content:center;background-color:lightgrey;">
+                    $greet
+                    <img src="/images/under-construction.gif" /></body>
+                </html>
+            EOF);
+        }
+}
 }
