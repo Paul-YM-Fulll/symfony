469d5f56d7ca519308bc33391f0c28d434390312
Paul Yves-Menager <paul.yves-menager@fulll.fr>
2023-10-03 10:08:16 +0000

    Customising easy admin

--- guestbook/src/Controller/Admin/CommentCrudController.php
+++ guestbook/src/Controller/Admin/CommentCrudController.php
@@ -12,14 +12,43 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+        public function configureCrud(Crud $crud): Crud
+        {
+            return $crud
+                ->setEntityLabelInSingular('Conference Comment')
+                ->setEntityLabelInPlural('Conference Comments')
+                ->setSearchFields(['author', 'text', 'email'])
+                ->setDefaultSort(['createdAt' => 'DESC'])
+            ;
+        }
+    
+        public function configureFilters(Filters $filters): Filters
+        {
+            return $filters
+                ->add(EntityFilter::new('conference'))
+            ;
+        }
+
+        public function configureFields(string $pageName): iterable
+     {
+        yield AssociationField::new('conference');
+        yield TextField::new('author');
+        yield EmailField::new('email');
+        yield TextareaField::new('text')
+            ->hideOnIndex();
+        yield TextField::new('photoFilename')
+            ->onlyOnIndex();
+        $createdAt = DateTimeField::new('createdAt')
+            ->setFormTypeOptions([
+                'html5' => true,
+                'years' => range(date('Y'), date('Y') + 5),
+                'widget' => 'single_text',
+            ]);
+
+        if (Crud::PAGE_EDIT === $pageName) {
+            yield $createdAt->setFormTypeOption('disabled', true);
+        } else {
+            yield $createdAt;
+        }
+     }
 }
